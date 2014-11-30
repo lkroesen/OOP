bb448bcfa21051d29704152f4cfff6afdb7ed4ae
DylanStraub <denver_dersen@hotmail.com>
2014-11-30 23:52:11 +0100

    fixed music and added comments

--- src/gui/javafx.java
+++ src/gui/javafx.java
@@ -1,25 +1,43 @@
 package gui;
+import java.io.File;
+
+//unused imports are unused
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Reflection;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayerBuilder;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaViewBuilder;
 import javafx.stage.Stage;
 
 
 public class javafx extends Application{
+	//looking into xml parser and video filling the background 
 	
+	//possible image for video still creating
+	//private static final String MEDIA_URL = "http://ak5.picdn.net/shutterstock/videos/3340358/preview/stock-footage-spinning-football-d-animation-stock-footage-clip-isolated-on-white-background-full-hd-video.mp4";
+    //private static String arg1;
+	
+    //toptext of every scene and button for starting screen
 	Label lbtext;
 	Button newgame, loadgame;
 	
+	//all buttons for teams to be made with xml-parser
 	Button ADO = new Button ("ADO");
 	Button Ajax = new Button ("Ajax");
 	Button AZ = new Button ("AZ");
@@ -40,23 +58,52 @@ public class javafx extends Application{
 	Button Zwolle = new Button ("Zwolle");
 	Button Back = new Button ("Back");
 	
-	Media BACKGROUND_MEDIA = new Media(MediaTest.class.getResource("Bioshock infinite rap.mp3").toString());
-	MediaPlayerBuilder builder;
 	
+	//launches the gui
 	public static void main(String[] args){
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		VBox root = new VBox();
-		HBox start = new HBox();
+		//song name in file form
+		File file = new File("C:/Users/Denver/oop/OOP/bin/fmsong.mp3");
 		
+		//plays the song endless
+		final String mediaLocation = file.toURI().toURL().toExternalForm();
+		Media song = new Media(mediaLocation);
+		MediaPlayer audio = new MediaPlayer(song);
+		audio.play();
+		audio.setCycleCount(audio.INDEFINITE);
+		
+		//first boxes for scene layout
+		VBox root = new VBox(10);
+		HBox start = new HBox();
+		//StackPane fm = new StackPane(); //for video
+		
+		//effects on buttons
 		Reflection reflection = new Reflection();
 		reflection.setFraction(0.8);
 		reflection.setTopOffset(-20);
 		
+		//Media media = new Media((arg1 != null) ? arg1 : MEDIA_URL);
+	    //MediaPlayer mediaPlayer = new MediaPlayer(media);
+	    //mediaPlayer.setAutoPlay(true);
+	    //mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+		//all video stuff
+	        
+	       
+	       /*MediaView mediaView = new MediaView(mediaPlayer);
+	       final DoubleProperty width = mediaView.fitWidthProperty();
+	       final DoubleProperty height = mediaView.fitHeightProperty();
+	       mediaView.setScaleY(1.5);
+	       
+	       width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
+	       height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
+	       also video stuff
+		*/
 		
+		//set the text for first buttons and label and giving effects
 		lbtext = new Label("Footballmanager");
 		newgame = new Button("new game");
 		loadgame = new Button("load game");
@@ -65,21 +112,25 @@ public class javafx extends Application{
 		
 		lbtext.getStyleClass().add("mycustomLabel");
 		
+		//makes a scene with the title setting it in the stage
 		Scene scene = new Scene(root,500,500);
-		stage.setTitle("Awesome game of awesomeness");
+		stage.setTitle("Footballmanager");
 		stage.setScene(scene);
-		
+		//fm.getChildren().addAll(mediaView,root); // video stuff
 		scene.getStylesheets().add("mystyle.css");
 		
-		
+		//shows current scene
 		stage.show();
 		
+		//actions by pressing new game button
 		newgame.setOnAction(new EventHandler<ActionEvent>(){
 			
 			@Override
 			public void handle(ActionEvent arg0){
+				//changes the text
 				lbtext.setText("Selection menu");
 				
+				//makes new boxes for layout new game screen
 				VBox teams1 = new VBox(10);
 				teams1.setAlignment(Pos.CENTER_LEFT);
 				VBox teams2 = new VBox(10);
@@ -88,6 +139,7 @@ public class javafx extends Application{
 				vbBack.setAlignment(Pos.BOTTOM_RIGHT);
 				Label empty = new Label(" ");
 				
+				//adds the buttons and the label and sets the scene in the stage
 				teams1.getChildren().addAll(lbtext,ADO,Ajax,AZ,Cambuur,Dordrecht,Excelsior,Feyenoord,GAEagles,Groningen);
 				teams2.getChildren().addAll(empty,Heracles,Heerenveen,NAC,PSV,Twente,Utrecht,Vitesse,WillemII,Zwolle);
 				vbBack.getChildren().addAll(Back);
@@ -97,10 +149,13 @@ public class javafx extends Application{
 				Scene ng = new Scene(toptext,500,500);
 				ng.getStylesheets().add("mystyle.css");
 				stage.setScene(ng);
+				
+				//back button actions
 				Back.setOnAction(new EventHandler<ActionEvent>(){
 					
 					@Override
 					public void handle(ActionEvent arg0){
+						//sets the old stage back possible error with calling new game screen again(testing)
 						VBox back = root;
 						back.getChildren().removeAll(newgame,loadgame);
 						lbtext.setText("Footballmanager");
@@ -113,16 +168,19 @@ public class javafx extends Application{
 			}
 			
 		});
-		
+		//actions for the load game button
 		loadgame.setOnAction(new EventHandler<ActionEvent>(){
 			
 			@Override
 			public void handle(ActionEvent arg0){
 				lbtext.setText("Select save game");
+				//makes the amount of saves visable as buttons
 				//for(int i = 1; i < 4/*savedgames + 1*/;i++){
 				//	Button save = new Button ("save " + i);
 				//	root.getChildren().add(save);
 				//}
+				
+				//making boxes and buttons for load game screen
 				Button save = new Button ("save 1");
 				VBox lgtext = new VBox();
 				VBox backpos = new VBox();
@@ -133,10 +191,13 @@ public class javafx extends Application{
 				lgtext.getStylesheets().add("mystyle.css");
 				Scene lg = new Scene(lgtext,500,500);
 				stage.setScene(lg);
+				
+				//back button actions
 				Backlg.setOnAction(new EventHandler<ActionEvent>(){
 					
 					@Override
 					public void handle(ActionEvent arg0){
+						//sets the old stage see back button from new game
 						VBox backlg = root;
 						backlg.getChildren().removeAll(newgame,loadgame);
 						lbtext.setText("Footballmanager");
@@ -148,22 +209,8 @@ public class javafx extends Application{
 			}
 			
 		});
-		
+		//first box getting children 
 		root.getChildren().addAll(lbtext, newgame, loadgame);
-		
-       this.builder = MediaPlayerBuilder.create().media(BACKGROUND_MEDIA).onEndOfMedia(new Runnable() {
-
-                    public void run() {
-                        MediaPlayer player = javafx.this.builder.build();
-                        player.play();
-                    }
-                });
-
-
-
-        MediaPlayer player = this.builder.build();
-        player.play();
-		
 		
 	}
 }
