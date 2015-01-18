592c35e79ca3a09c0727521949d1929c4608fe37
DylanStraub <denver_dersen@hotmail.com>
2015-01-19 00:18:13 +0100

    mistakes boys

--- src/xml/XML.java
+++ src/xml/XML.java
@@ -350,10 +350,6 @@ public class XML {
 						position.setTextContent(p.getPosition() + "");
 						ePlayer.appendChild(position);
 
-						Element position = doc.createElement("position");
-						position.setTextContent(p.getPosition() + "");
-						ePlayer.appendChild(position);
-
 						// Add player to team
 						eTeam.appendChild(ePlayer);
 					}
