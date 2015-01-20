4b8cc8a7f3976454b669dd031b021db8b01baceb
Tom <tom@MacBook-Pro-van-Tom-2.local>
2015-01-20 22:31:54 +0100

    added switching out players and made is so betting-test works without removing code from playmatch

--- src/AI/PlayMatch.java
+++ src/AI/PlayMatch.java
@@ -25,6 +25,8 @@ public class PlayMatch {
 	private static Match amatch;
 	private static ArrayList<Player> hplayers;
 	private static ArrayList<Player> aplayers;
+	private static ArrayList<Player> hreserve;
+	private static ArrayList<Player> areserve;
 	private static int hgoal;
 	private static int agoal;
 	
@@ -32,11 +34,11 @@ public class PlayMatch {
 		attackn = 0;
 		amatch = match;
 		Team home = match.getTeam_home();
-		hplayers = home.getPlayers();
+		//hplayers = home.getPlayers();
 		//System.out.println(home.getPlayers());
 		Team away = match.getTeam_away();
-		aplayers = away.getPlayers();
-		generatestats(hplayers,aplayers);
+		//aplayers = away.getPlayers();
+		generatestats(amatch);
 		
 		
 		
@@ -60,22 +62,22 @@ public class PlayMatch {
 			//System.out.println(hplayers);
 			for(Player player:hplayers){
 				//System.out.println(player.getPosition() + ":" + pos);
-				if(player.getPosition() == pos){
+				if(player.getType() == pos){
 					posplayers.add(player);
 				}
 			}
-			//int pid = posplayers.get(new Random().nextInt(posplayers.size())).getPosition();
-			//amatch.addEventHome(new Event(pid, id, time, 0));
+			int pid = posplayers.get(new Random().nextInt(posplayers.size())).getId();
+			amatch.addEventHome(new Event(pid, id, time, 0));
 		}else{
 			ArrayList<Player> posplayers = new ArrayList<Player>();
 			for(Player player:aplayers){
 				//System.out.println(player.getPosition() + ":" + pos);
-				if(player.getPosition() == pos){
+				if(player.getType() == pos){
 					posplayers.add(player);
 				}
 			}
-			//int pid = posplayers.get(new Random().nextInt(posplayers.size())).getPosition();
-			//amatch.addEventAway(new Event(pid, id, time, 0));
+			int pid = posplayers.get(new Random().nextInt(posplayers.size())).getId();
+			amatch.addEventAway(new Event(pid, id, time, 0));
 		}
 		
 	}
@@ -93,7 +95,7 @@ public class PlayMatch {
 				t = 100;
 			}
 			if(random(t,100)){
-				addevent(3,true,-1);
+				addevent(3,true,3);
 				hgoal += 1;
 			}
 		}else{
@@ -104,29 +106,81 @@ public class PlayMatch {
 				t = 100;
 			}
 			if(random(t,100)){
-				addevent(3,false,-1);
+				addevent(3,false,3);
 				agoal += 1;
 			}
 		}
 		
 		//yellow card
-		if(random(36.36,100)){
+		if(random(36.36,63.64)){
 			int pos = randompos(2, 33, 35, 11);
 			if(pos == 3){
-				addevent(1, i, -1);
+				addevent(1, i, pos);
 			}else{
-				addevent(1, !i, -1);
+				addevent(1, !i, pos);
 			}
 		}
-		
-		
+		wissel();
 	}
 	
 //	private void yellowcard(boolean i){
 //		
 //	}
 
-	//returns true if the first double wins
+	private static void wissel(){
+		if(random(45,55)){
+			int pos = randompos(0.2, 0.4, 1.27, 1);
+			int time = (int) Math.round((new Random().nextDouble() * 8.18) + (attackn * 8.18)); 
+			if(random(1, 1)){
+				ArrayList<Player> hr =  new ArrayList<Player>();
+				ArrayList<Player> hb =  new ArrayList<Player>();
+				for(Player p:hplayers){
+					if(p.getType() == pos)
+						hb.add(p);
+				}
+				for(Player p:hr){
+					if(p.getType() == pos)
+						hr.add(p);
+				}
+				if(hr.size() == 0 || hb.size() == 0){
+					return;
+				}
+				Player pr = hr.get(new Random().nextInt(hr.size()));
+				Player pb = hb.get(new Random().nextInt(hb.size()));
+				int tp = pr.getPosition();
+				pr.setPosition(pb.getPosition());
+				pb.setPosition(tp);
+				amatch.addEventHome(new Event(pr.getId(), 5, time, 0));
+				amatch.addEventHome(new Event(pb.getId(), 4, time, 0));
+				generatestats(amatch);
+			}else{
+				ArrayList<Player> ar =  new ArrayList<Player>();
+				ArrayList<Player> ab =  new ArrayList<Player>();
+				for(Player p:aplayers){
+					if(p.getType() == pos)
+						ab.add(p);
+				}
+				for(Player p:ar){
+					if(p.getType() == pos)
+						ar.add(p);
+				}
+				if(ar.size() == 0 || ab.size() == 0){
+					return;
+				}
+				Player pr = ar.get(new Random().nextInt(ar.size()));
+				Player pb = ab.get(new Random().nextInt(ab.size()));
+				int tp = pr.getPosition();
+				pr.setPosition(pb.getPosition());
+				pb.setPosition(tp);
+				amatch.addEventAway(new Event(pr.getId(), 5, time, 0));
+				amatch.addEventAway(new Event(pb.getId(), 4, time, 0));
+				generatestats(amatch);
+			}
+		}
+	}
+	
+	
+	//returns true if the first double win
 	private static boolean random(double x, double y){
 		double i = x + y;
 		double r = new Random().nextDouble() * i;
@@ -152,7 +206,28 @@ public class PlayMatch {
 		}
 	}
 	
-	private static void generatestats(ArrayList<Player> hplayers, ArrayList<Player> aplayers){
+	private static void generatestats(Match match){
+		hplayers = new ArrayList<Player>();
+		aplayers = new ArrayList<Player>();
+		hreserve = new ArrayList<Player>();
+		areserve = new ArrayList<Player>();
+		
+		for(Player player:match.getTeam_away().getPlayers()){
+			if(player.getPosition()<=10){
+				aplayers.add(player);
+			}else if(player.getPosition() <= 17){
+				areserve.add(player);
+			}
+		}
+		
+		for(Player player:match.getTeam_home().getPlayers()){
+			if(player.getPosition()<=10){
+				hplayers.add(player);
+			}else if(player.getPosition() <= 17){
+				hreserve.add(player);
+			}
+		}
+		
 		hstamina = getStamina(hplayers);
 		hattack = getOffenciveScore(hplayers);
 		hdefend = getDefenciveScore(hplayers);
