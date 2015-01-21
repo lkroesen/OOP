e905bd683352281a1f20b0960f40f49a71a70401
Joris Oudejans <joris.oudejans@gmail.com>
2015-01-21 15:26:09 +0100

    Added currentPlayRound to game. TODO update GameTest

--- src/model/Game.java
+++ src/model/Game.java
@@ -17,23 +17,43 @@ public class Game {
 	private String name;
 	private static int currentDay;
 	private static int currentTeam;
+	private static int currentLeague;
+	private static int curentPlayRound;
 	private ArrayList<League> leagues;
 	private ArrayList<Transfer> transfers;
 	private ArrayList<Match> matches;
 	private Schedule schedule;
 
-	public Game(int id, String name, int currentDay, int currentTeam, Schedule schedule) {
+	public Game(int id, String name, int currentDay, int currentTeam, int currentLeague, int currentPlayRound, Schedule schedule) {
 		super();
 		this.id = id;
 		this.name = name;
 		Game.currentDay = currentDay;
 		Game.currentTeam = currentTeam;
+		Game.currentLeague = currentLeague;
+		Game.curentPlayRound = currentPlayRound;
 
 		this.schedule = schedule;
 
 		leagues = new ArrayList<League>();
 		transfers = new ArrayList<Transfer>();
 		matches = new ArrayList<Match>();
+	}
+
+	public static int getCurentPlayRound() {
+		return curentPlayRound;
+	}
+
+	public static void setCurentPlayRound(int curentPlayRound) {
+		Game.curentPlayRound = curentPlayRound;
+	}
+
+	public static int getCurrentLeague() {
+		return currentLeague;
+	}
+
+	public static void setCurrentLeague(int currentLeague) {
+		Game.currentLeague = currentLeague;
 	}
 
 	public Schedule getSchedule() {
