081b251d9a216c9254ead2fc1b52aad8325e1d30
Dennis de Jong <dennisdejong1995@gmail.com>
2015-01-25 22:29:48 +0100

    lege testTransferalgoritme gemaakt

---
+++ src/AI/TransferAlgorithmTest.java
@@ -0,0 +1,14 @@
+package AI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransferAlgorithmTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
