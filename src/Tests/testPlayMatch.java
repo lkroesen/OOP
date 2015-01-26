d9404b36273a1388a0a577d6c411f287623b80c9
Tom <tom@MacBook-Pro-van-Tom-2.local>
2015-01-26 16:20:35 +0100

    fixed playmatchtest

--- src/Tests/testPlayMatch.java
+++ src/Tests/testPlayMatch.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import model.League;
 import model.Match;
 import model.Player;
 import model.Team;
@@ -16,6 +17,7 @@ public class testPlayMatch {
 
 	@Test
 	public void testMatch() {
+		League l = new League(1, "test", "erens");
 		Team t1 = new Team(1,"Ajax");
 		ArrayList<Player> pl = new ArrayList<Player>();
 		for (int c = 1; c < 15; c++){
@@ -23,10 +25,12 @@ public class testPlayMatch {
 		t1.addPlayer(new Player(c, "Ha", "Ha", (byte) 1, c, c, (byte) 50, (byte) 50, (byte) 50, 5));}
 		
 		Team t2 = t1;
+		l.addTeam(t2);
+		l.addTeam(t1);
 		
 		
-		Match match = new Match(1,1,t1,t2);
-		match.setResult(PlayMatch.play(match));
+		Match match = new Match(1,1,t1.getId(),t2.getId());
+		match.setResult(PlayMatch.play(match, l));
 		assertNotNull(match.getResult());
 	}
 
