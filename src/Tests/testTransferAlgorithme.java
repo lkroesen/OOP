448c9e78480bf3a74e6df9119269cc966c1838d9
Dennis de Jong <dennisdejong1995@gmail.com>
2015-01-26 17:01:01 +0100

    testTransferalgo

--- src/Tests/testTransferAlgorithme.java
+++ src/Tests/testTransferAlgorithme.java
@@ -15,6 +15,8 @@ import model.Game;
 
 import org.junit.Test;
 
+import xml.XML;
+
 public class testTransferAlgorithme {
 
 	@Test
@@ -143,4 +145,39 @@ public class testTransferAlgorithme {
 		assertNotNull(t);
 	}
 	
+	
+	@Test
+	public void testCalculateWorth(){
+		ArrayList<Team> teams = new ArrayList<Team>();
+		for (int c = 0; c < 18; c++){String a = "team " + c;Team team = new Team(c, a);teams.add(c, team);}
+		League leaguee = new League(0, "Eredivisie", "Nederland", teams);
+		//  ID       NAME        SURNAME        JERSEY_NR  	TYPE   POS  OFFENSIVE   DEFENSIVE  STAMINA       PRICE
+		Player one    = new Player	(1, 	"Neo", 		"Keeper", 		(byte)  1, 	 0, 	0, 	(byte)  20, (byte) 69, (byte) 56, 	 1000);
+		Schedule s = Scheduler.scheduler(leaguee);
+		Game g = new Game(1, "game", 0, 1, 0, 0, s);
+		TransferAlgorithm t = new TransferAlgorithm(g);
+		
+		t.CalculateWorth(one);
+		int worth = 10*(one.getDefensiveScore()*one.getOffensiveScore()*one.getStaminaScore());
+		
+		assertEquals(one.getPrice(), worth);
+	}
+	
+	@Test
+	public void testTransfer(){
+		ArrayList<Team> teams = new ArrayList<Team>();
+		for (int c = 0; c < 18; c++){String a = "team " + c;Team team = new Team(c, a);teams.add(c, team);}
+		League leaguee = new League(0, "Eredivisie", "Nederland", teams);
+		//  ID       NAME        SURNAME        JERSEY_NR  	TYPE   POS  OFFENSIVE   DEFENSIVE  STAMINA       PRICE
+		Player one    = new Player	(1, 	"Neo", 		"Keeper", 		(byte)  1, 	 0, 	0, 	(byte)  20, (byte) 69, (byte) 56, 	 1000);
+		Schedule s = Scheduler.scheduler(leaguee);
+		Game g = new Game(1, "game", 0, 1, 0, 0, s);
+		TransferAlgorithm t = new TransferAlgorithm(g);
+		t.AddPlayer(one);
+		ArrayList<Player> ar = new ArrayList();
+		ar.add(one);
+		assertEquals(t.getTransferringplayers(), ar);
+		
+		
+	}
 }
